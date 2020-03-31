c1597cf600949586ac4f43cf064483a7e2bd1423
JesusRRr <rolonjesus1996@gmail.com>
2020-03-31 16:34:21 -0600

    feature: add searchSection panel

--- src/com/hcl/library/Index.java
+++ src/com/hcl/library/Index.java
@@ -1,34 +1,32 @@
 package com.hcl.library;
 
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
+import javax.swing.GroupLayout;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTable;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
-import javax.swing.plaf.metal.MetalBorders;
 
-import com.sun.org.apache.xpath.internal.axes.SelfIteratorNoPredicate;
-
+@SuppressWarnings("serial")
 public class Index extends JFrame {
+	private JPanel searchSection;
 	
 	public Index() {
 		initComponents();
 	}
 	
 	private void initComponents() {
+		searchSection = new JPanel();
+		
+		//JFrame configuration 
 		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		this.setSize(800, 500);
+		this.setSize(800,500);
+		this.setLayout(null);
+		
+		//searchSection Panel configuration
+		searchSection.setSize(800,100);
+		searchSection.setBackground(Color.DARK_GRAY);
+		this.add(searchSection);
 	}
 	
 	public static void main(String[] args) {
