65eb76e2bf9096c91af07df44ccf062ad8632cdd
JesusRRr <rolonjesus1996@gmail.com>
2020-03-31 15:55:33 -0600

    fix: refactor Index class

--- src/com/hcl/library/Index.java
+++ src/com/hcl/library/Index.java
@@ -1,31 +1,26 @@
 package com.hcl.library;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.MetalBorders;
 
-public class Index extends JFrame{
+public class Index extends JFrame {
+	
 	public Index() {
-		this.setTitle("Index");
-		this.setLocation(200,100);
-		this.setSize(800,500);
-		this.setVisible(true);
-		this.setResizable(false);
-		components();
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-	
-	private void components() {
-		JPanel mainPanel = new JPanel();
-		JLabel searchLabel = new JLabel();
 		
-		mainPanel.setBackground(Color.DARK_GRAY);
-		
-		this.getContentPane().add(mainPanel);
 	}
-	
-	
 
 }
