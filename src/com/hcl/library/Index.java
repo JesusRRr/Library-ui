63d23be5c6932d0ee0603a84dc7b7e956bad63eb
JesusRRr <rolonjesus1996@gmail.com>
2020-04-07 11:41:28 -0500

    feature: view package created and TablesView was moved to there

--- src/com/hcl/library/Index.java
+++ src/com/hcl/library/Index.java
@@ -1,5 +1,7 @@
 package com.hcl.library;
 
+import com.hcl.library.ui.view.TablesView;
+
 public class Index {
 	public static void main(String[] args) {
 		new TablesView().setVisible(true);
--- src/com/hcl/library/ui/view/TablesView.java
+++ src/com/hcl/library/ui/view/TablesView.java
@@ -1,4 +1,4 @@
-package com.hcl.library;
+package com.hcl.library.ui.view;
 
 import java.awt.Color;
 import java.awt.Component;
