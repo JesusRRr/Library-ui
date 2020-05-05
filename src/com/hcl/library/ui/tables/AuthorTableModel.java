900d9da7834c9040f4a0fe0c0e4791dbdce70a50
JesusRRr <rolonjesus1996@gmail.com>
2020-05-05 10:32:56 -0500

    feature: get authors from database and place them into AuthorTableModel

--- src/com/hcl/library/ui/tables/AuthorTableModel.java
+++ src/com/hcl/library/ui/tables/AuthorTableModel.java
@@ -1,7 +1,15 @@
 package com.hcl.library.ui.tables;
 
+import java.util.List;
+
+import com.hcl.library.model.po.AuthorPO;
+import com.hcl.library.model.po.BookPO;
+import com.hcl.library.service.AuthorService;
+import com.hcl.library.service.BookService;
+
 @SuppressWarnings("serial")
-public class AuthorTableModel extends PersonTableModel{
+public class AuthorTableModel extends EntityTableModel{
+	private String[] fields= {"id","Name","LastName","Nationality"};
 	
 	public AuthorTableModel() {
 		addColumns();
@@ -9,10 +17,27 @@ public class AuthorTableModel extends PersonTableModel{
 	}
 	
 	void addColumns() {
-		this.addColumn("Nationality");
+		for(String field: fields) {
+			this.addColumn(field);
+		}
 	}
 	
 	void addRows() {
-		
+		refreshData();
+	}
+	
+	private void refreshData() {
+		  List<AuthorPO> authors= AuthorService.getInstance().findAll();
+
+		  Object[][] authorsAsObjects = new  Object[authors.size()][fields.length];
+		  for(int row=0;row<authors.size();row++) {
+			  authorsAsObjects[row][0]=authors.get(row).getId();
+			  authorsAsObjects[row][1]=authors.get(row).getName();
+			  authorsAsObjects[row][2]=authors.get(row).getLastName();
+			  authorsAsObjects[row][3]=authors.get(row).getNacionality();
+	 
+			  this.addRow(authorsAsObjects[row]);
+		  }
+		 
 	}
 }
