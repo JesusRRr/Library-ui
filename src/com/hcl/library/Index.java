5c52f5a3ee97cbb408d163e75af2b35660e07126
JesusRRr <rolonjesus1996@gmail.com>
2020-05-08 15:10:36 -0500

    chore: create MainView class

--- src/com/hcl/library/Index.java
+++ src/com/hcl/library/Index.java
@@ -1,13 +1,15 @@
 package com.hcl.library;
 
 import com.hcl.library.ui.view.BookView;
+import com.hcl.library.ui.view.MainView;
 import com.hcl.library.ui.view.TablesView;
 import com.hcl.library.ui.view.forms.BookFormView;
 
 public class Index {
 	public static void main(String[] args) {
-		new TablesView().setVisible(true);
-		new BookFormView().setVisible(false);
+		//new TablesView().setVisible(true);
+		//new BookFormView().setVisible(false);
 		//new BookView().setVisible(false);
+		new MainView().setVisible(true);
 	}
 }
---
+++ src/com/hcl/library/ui/view/MainView.java
@@ -0,0 +1,32 @@
+package com.hcl.library.ui.view;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+@SuppressWarnings("serial")
+public class MainView extends JFrame{
+	private JPanel mainPanel;
+	private JPanel navBarPanel;
+	private JPanel mainImagePanel;
+	
+	public MainView() {
+		this.setSize(1000,500);
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		mainPanel=new JPanel(null);
+		mainPanel.setSize(1000, 500);
+		mainPanel.setBackground(Color.BLACK);
+		this.add(mainPanel);
+		
+		initComponents();
+	}
+	
+	private void initComponents() {
+		navBarPanel= new JPanel();
+		navBarPanel.setSize(1000, 50);
+		mainPanel.add(navBarPanel);
+	}
+	
+}
