106e724f5aa34b0716f21db050f7bcc67fad4666
JesusRRr <rolonjesus1996@gmail.com>
2020-05-11 16:19:58 -0500

    chore: CustomerField class created

---
+++ src/com/hcl/library/ui/view/fields/CustomerField.java
@@ -0,0 +1,8 @@
+package com.hcl.library.ui.view.fields;
+
+public class CustomerField extends EntityField{
+	
+	public CustomerField(String field, int size) {
+		super(field,size);
+	}
+}
--- src/com/hcl/library/ui/view/forms/AddressFieldPanel.java
+++ src/com/hcl/library/ui/view/forms/AddressFieldPanel.java
@@ -6,7 +6,7 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class AddressFieldPanel extends JPanel{
-	
+	private CustomerField customerField;
 	
 	public AddressFieldPanel() {
 		initComponents();
