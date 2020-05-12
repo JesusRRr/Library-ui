c95caee377ee2a9766ae0d4dd0ba9cc5fd198f75
JesusRRr <rolonjesus1996@gmail.com>
2020-05-12 14:07:10 -0500

    feature: retrieve customer from database

--- src/com/hcl/library/ui/tables/CustomerTableModel.java
+++ src/com/hcl/library/ui/tables/CustomerTableModel.java
@@ -1,8 +1,13 @@
 package com.hcl.library.ui.tables;
 
+import java.util.List;
+
+import com.hcl.library.model.po.CustomerPO;
+import com.hcl.library.service.CustomerService;
 
 @SuppressWarnings("serial")
 public class CustomerTableModel extends PersonTableModel{
+	private String[] fields= {"id","Name","LastName","Curp","Birhday","Phone","Email","address"};
 	
 	public CustomerTableModel() {
 		addColumns();
@@ -11,18 +16,37 @@ public class CustomerTableModel extends PersonTableModel{
 
 	
 	void addColumns() {
-		this.addColumn("Phone");
-		this.addColumn("email");
-		this.addColumn("status");
+		for(String field: fields) {
+			this.addColumn(field);
+		}
 	
 	}
 	
 	void addRows() {
-		
-		/*TO-DO
-		 *1.-get customer from database 
-		 *2.-Do a for-each and addRow to table
-		*/
-	
+		refreshData();
 	}
+	
+	private void refreshData() {
+		  List<CustomerPO> customers= CustomerService.getInstance().findAll();
+
+		  Object[][] CustomerAsObjects = new  Object[customers.size()][fields.length];
+		  for(int row=0;row<customers.size();row++) {
+			  CustomerAsObjects[row][0]=customers.get(row).getId();
+			  CustomerAsObjects[row][1]=customers.get(row).getName();
+			  CustomerAsObjects[row][2]=customers.get(row).getLastName();
+			  CustomerAsObjects[row][3]=customers.get(row).getCurp();
+			  CustomerAsObjects[row][4]=customers.get(row).getBirthday();
+			  CustomerAsObjects[row][5]=customers.get(row).getPhone();
+			  CustomerAsObjects[row][6]=customers.get(row).getEmail();
+			  CustomerAsObjects[row][7]=new StringBuilder(
+					  customers.get(row).getAddress().getStreet())
+					  .append(" ")
+					  .append(customers.get(row).getAddress().getNumber());
+				  
+			  this.addRow(CustomerAsObjects[row]);
+			  this.getTableModelListeners();
+		  }
+		 
+	}
+	
 }
--- src/com/hcl/library/ui/tables/PersonTableModel.java
+++ src/com/hcl/library/ui/tables/PersonTableModel.java
@@ -3,8 +3,8 @@ package com.hcl.library.ui.tables;
 @SuppressWarnings("serial")
 public abstract class  PersonTableModel extends EntityTableModel{
 	public PersonTableModel() {
-		Columns();
-		Rows();
+		
+		
 	}
 
 	
@@ -15,27 +15,5 @@ public abstract class  PersonTableModel extends EntityTableModel{
 		this.addColumn("Curp");
 	
 	}
-	
-	void Rows() {
-		
-		/*TO-DO
-		 *1.-get book from database 
-		 *2.-Do a for-each and addRow to table
-		
-		*/
-		String[] row= {"1","lord of the rings", "2450"};
-		String[] row1= {"1","lord of the rings", "2450"};
-		String[] row2= {"1","lord of the rings", "2450"};
-		String[] row3= {"1","lord of the rings", "2450"};
-		String[] row4= {"1","lord of the rings", "2450"};
-		String[] row5= {"1","lord of the rings", "2450"};
-	
-		this.addRow(row);
-		this.addRow(row1);
-		this.addRow(row2);
-		this.addRow(row3);
-		this.addRow(row4);
-		this.addRow(row5);
-		
-	}
+
 }
