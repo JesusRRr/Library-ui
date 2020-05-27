ef2b9655e94a20c8fb5120a73a70c85b512fa2e4
JesusRRr <rolonjesus1996@gmail.com>
2020-05-27 13:49:14 -0500

    feature: CustomerSelector class finished

--- src/com/hcl/library/ui/comboboxes/CustomerSelector.java
+++ src/com/hcl/library/ui/comboboxes/CustomerSelector.java
@@ -1,8 +1,33 @@
 package com.hcl.library.ui.comboboxes;
 
+import java.util.List;
+
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 
+import com.hcl.library.model.po.CustomerPO;
+import com.hcl.library.service.CustomerService;
+
 @SuppressWarnings("serial")
-public class CustomerSelector extends JComboBox<String>{
+public class CustomerSelector extends JComboBox<CustomerPO>{
+	
+	public CustomerSelector() {
+		this.setModel(new DefaultComboBoxModel<CustomerPO>(getCustomers()));
+	}
+	
+	public CustomerPO[] listToArray(List<CustomerPO> customers) {
+		CustomerPO[] array = new CustomerPO[customers.size()];
+		int index=0;
+		
+		for(CustomerPO customer : customers) {
+			array[index++]=customer;
+		}
+		return array;
+	}
+	
+	public CustomerPO[] getCustomers() {
+		
+		return listToArray(CustomerService.getInstance().findAll());
+	}
 
 }
